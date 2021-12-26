2971e55ca0c078fa07466e747fa43ca326c8edd7
JAGDISH0008 <jagadeesh26597@gmail.com>
2021-12-26 21:43:45 +0530

    web3 package added.

--- blockchain/helloworld/front-end/src/app/app.component.ts
+++ blockchain/helloworld/front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Web3 from 'web3';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello-world';
   message: string = "";
+  web3 = new Web3('ws://localhost:8546');
   onSubmit() {
     console.log(this.message);
   }
--- blockchain/helloworld/front-end/src/polyfills.ts
+++ blockchain/helloworld/front-end/src/polyfills.ts
@@ -67,3 +67,13 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import { Buffer } from 'buffer';
+
+(window as any).global = window;
+global.Buffer = Buffer;
+global.process = {
+  env: { DEBUG: undefined },
+  version: '',
+  nextTick: require('next-tick')
+} as any;
