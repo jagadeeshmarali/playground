2bdbdd73c0d4108fab3d4390d82498ac5e329556
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-02-03 09:26:16 +0530

    initial project setup.

--- blockchain/account-fetch/src/app/app.component.ts
+++ blockchain/account-fetch/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ethers } from "ethers";
+declare const window: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'account-fetch';
+  constructor() {
+    this.init();
+
+  }
+
+  async init() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    await window.ethereum.enable()
+    let accounts = await provider.listAccounts();
+    let balance = await provider.getBalance(accounts[0]);
+    console.log(ethers.utils.formatEther(balance));
+  }
 }
