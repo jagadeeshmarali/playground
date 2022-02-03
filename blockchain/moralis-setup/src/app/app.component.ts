783205018c3203c5502d0c46b255847c1b209c2a
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-02-03 11:19:06 +0530

    login added.

--- blockchain/moralis-setup/src/app/app.component.ts
+++ blockchain/moralis-setup/src/app/app.component.ts
@@ -1,5 +1,7 @@
+
 import { Component } from '@angular/core';
 import { Moralis } from 'moralis';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,37 @@ import { Moralis } from 'moralis';
 })
 export class AppComponent {
   title = 'moralis-setup';
+  public user: any;
+  public isLoggedIn = false;
+  public createdAt: any;
+  public updatedAt: any;
+  public ethAddress: any;
+  public isAuthenticated = false;
+  private moralis = Moralis;
+  constructor() {
+    this.moralis.start({ serverUrl: environment.moralisServerUrl, appId: environment.moralisAppId });
+  }
+
+
+
+  // Login 
+  async login() {
+    this.user = await this.moralis.User.current();
+    if (!this.user) {
+      let user = await Moralis.authenticate();
+      console.log("login ==> ", user);
+      this.createdAt = user.createdAt;
+      this.updatedAt = user.updatedAt;
+      // this.ethAddress = user.attributes.ethAddress;
+    }
+    this.isLoggedIn = true;
+  }
+
+  // Logout
+  async logOut() {
+    await this.moralis.User.logOut();
+    console.log("logged out ==>");
+    this.isLoggedIn = false;
+  }
+
 }
