92633195529ebb5751825f2e25140efe03e743f9
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-02-03 10:52:26 +0530

    environments files moved to gitignore and moralis package added.

--- blockchain/moralis-setup/src/app/app.component.ts
+++ blockchain/moralis-setup/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Moralis } from 'moralis';
 
 @Component({
   selector: 'app-root',
---
+++ blockchain/moralis-setup/src/environments/environment.sample.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  moralisAppId: "YOUR_APP_ID",
+  moralisServerUrl: "YOUR_SERVER_URL"
+};
+
