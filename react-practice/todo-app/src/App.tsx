4d7475595325e1e15d9aca2c4db985f980206700
JAGDISH0008 <jagadeesh26597@gmail.com>
2023-02-28 20:30:31 -0800

    react practice start.

---
+++ react-practice/todo-app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import INPUT_FIELD from './components/input';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       TODO APP
+//       <INPUT_FIELD/>
+//     </div>
+//   );
+// }
+const App:React.FC = ()=>{
+  return (
+    <div className="App">
+      TODO APP
+      <INPUT_FIELD/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practice/todo-app/src/components/input.tsx
@@ -0,0 +1,8 @@
+export default function INPUT_FIELD():JSX.Element {
+  return (
+    <form>
+      <input type="text" placeholder="Enter Task"></input>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
